0907580ebb51daddd8987dc4ffa6c61517450a91
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-07 23:17:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.get('/ui/SINGH_ASHUTOSH.jpg', function (req, res) {
 
 var counter=1;
 app.get('/ui/article-one.html', function (req, res) {
- // res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
- // counter=counter+1;
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  counter=counter+1;
   res.send(counter.toString());
 });
 
