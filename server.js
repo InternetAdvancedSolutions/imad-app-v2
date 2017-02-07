1ec6569885d7c8c6ac3ca2f373cb9605e0d983eb
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-07 23:11:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+var counter=1;
 app.get('/ui/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  counter=counter+1;
+  res.send(counter.toString());
 });
 
 app.get('/ui/article-two.html', function (req, res) {
