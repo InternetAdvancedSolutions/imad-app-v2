0fe75fd521e92f7d83681fec0088deaadd1018bf
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-09 00:21:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,17 @@
 //console.log('Loaded!');
-var button=document.getElementById("homeB");
+var button=document.getElementById("homeb");
 
 button.onclick=function(){
     var request= new XMLHttpRequest();
     
     request.onreadystatechange=function(){
-        if(request.readystate===request.DONE && request.status===200)
-        {var count =request.responseText;
-            var span=document.getElementById("sp");
-            span.innerHTML=count.toString();
+        if(request.readystate===request.DONE)
+        {if(request.status===200)
+           {
+             var count =request.responseText;
+             var span=document.getElementById("sp");
+             span.innerHTML=count.toString();
+        }
         }
     };
     
