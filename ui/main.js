7a12387abdcbad8ea0d70f110b65a316386955e4
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-08 23:25:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+var button=document.getElementById("homeB");
+
+button.onclick=function(){
+    var request= new XMLHttpRequest();
+    
+    request.readystatechange=function(){
+        if(request.readystate==4 && request.status==200)
+        {var count =request.responseText;
+            var span=document.getElementById("sp");
+            span.innerHTML=count.toString();
+        }
+    };
+    
+    request.open('GET',"http://internetadvancedsolutions.imad.hasura-app.io/counter",true);
+    request.send(null);
+}
