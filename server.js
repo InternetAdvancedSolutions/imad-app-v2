5605bce1363d3cb19d5f63d98006019760501c0d
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-16 21:08:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,21 +121,7 @@ app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
-/*
-app.get('/ui/article-one.html', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
- 
-});
 
-app.get('/ui/article-two.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/ui/article-three.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
-*/
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
