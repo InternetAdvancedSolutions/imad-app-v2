6dd5fd048ae6cc511097f67beef392f4d16295e3
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-16 18:12:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,6 +31,76 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmltemplate=`
+    
+<!doctype html>
+<html>
+    <head>
+        <title>${title}</title>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width-device-width, initial-scale-1">
+    </head>
+    <body>
+    <div class="container" >   
+        <div id="logo" class="center" >
+        <table><tr><td>
+        <img src="ui/SINGH_ASHUTOSH.jpg" class="img-medium" aligh="center" ></td>
+        <td><h2 id="h1" class="center text-big bold">Internet Advanced Solutions (IAS)</h2>
+        <h3 class="center text-big bold">${heading}</h3></td><td><h4 id="h2"><span id="sp"></span> </h4></td></tr>
+        </table> 
+        <hr>
+        <pre class="fontcolor1">
+        <span class="fontcolor1" align="right" id="t"></span> |  <a class="hyperlinks" href="/ui/article-one.html">Webpps</a> |  <a class ="hyperlinks" href="ui/article-two.html">Database MS</a> |  <a class ="hyperlinks" href="ui/article-three.html">Single Page Apps</a>  |  <a class ="hyperlinks" href="/counter">Counter</a> <hr> 
+        </pre>
+       </div>
+       
+        <div id="main" class="center text-big bold">
+        Hi! I am Ashutosh 
+        </div>
+        
+        <h4 id="h" class="center">Internet Consultant and App Developer</h4>
+        <div id="sub" class="left">
+        I help startups develop and maintain Webapps, SPAs (single page apps), Websites and Database Management Systems.
+             
+            My fields of expertize are:
+            <ol type="i">
+            <li>C, Embedded C and C++</li>
+            <li>Java (J2EE)</li>
+            <li>HTML and CSS</li>
+            <li>PHP, javaScript and Node JS</li>
+            <li>SQL and Data modelling</li>
+            <li>Android Programming</li>
+            </ol>
+            </div>
+            <div id="d" class="center">
+            
+              A certified programmer from IIT kharagpur and B.Tech from Kanpur.
+            
+            </div>
+            <br>
+            
+        <div id="feedback">
+            <p id="p1">Share your views with me :</p>
+            <pre>
+            <textarea id="comments" cols="50" rows="10" maxlength="500"></textarea><br>
+            <button id="submit" >Post</button>
+            <p id="p2"></p>
+           </pre>
+        </div>
+       
+        <script src="/ui/main.js"></script>
+    </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+}
 app.get('/ui/article-one.html', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
  
