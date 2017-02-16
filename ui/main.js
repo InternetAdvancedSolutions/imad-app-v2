54e2aa2a55a9a40dda23d3ae71b776fcaf407da4
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-17 02:12:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,35 @@
   //for date and time
  // function time(){document.getElementById("t").innerHTML=Date();}
            // window.onload=time;
-  //for textarea  
+  //for textarea  for home page
   var button=document.getElementById("submit");
             var textarea=document.getElementById("comments");
             var para=document.getElementById("p2");
             button.onclick=function(){
                 para.innerHTML=textarea.value;};
-   // for Ajax data call of 'click me' button        
+                
+   //for textarea  for articleOne page
+  var button=document.getElementById("submit1");
+            var textarea=document.getElementById("comments1");
+            var para=document.getElementById("p2");
+            button.onclick=function(){
+                para.innerHTML=textarea.value;};
+                
+    //for textarea  for articleTwo page
+  var button=document.getElementById("submit2");
+            var textarea=document.getElementById("comments2");
+            var para=document.getElementById("p2");
+            button.onclick=function(){
+                para.innerHTML=textarea.value;};  
+                
+    //for textarea  for articleThree page
+  var button=document.getElementById("submit3");
+            var textarea=document.getElementById("comments3");
+            var para=document.getElementById("p2");
+            button.onclick=function(){
+                para.innerHTML=textarea.value;};
+                
+   // for Ajax data call of home page visit count       
    var button1=document.getElementById("homeB");
 
    // button1.onclick=function()
