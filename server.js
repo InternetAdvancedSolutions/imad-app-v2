6d972bcb0818a2aeda1a7a885354b42551e4963c
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-17 00:32:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-var port = 8080; // Use 8080 for local development because you might already have apache running //on 80
-app.listen(8080, function () {
+var port = 80; // Use 8080 for local development because you might already have apache running //on 80
+app.listen(80, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
