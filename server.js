a4d4d882bad746394a9c699034277ec9af9b1696
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-25 13:41:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get('/test-db', function(req,res){
 });//get ends here
 
 app.get('/articles/:articleName',function(req,res){
-    pool.query("SELECT * FROM article WHERE tile="+req.params.articleName, function(err,result){
+    pool.query("SELECT * FROM article WHERE title="+req.params.articleName, function(err,result){
         if(err)
         {
             res.status(500).send(err.toString());
