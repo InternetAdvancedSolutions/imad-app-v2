08ba6ebd671259091723d0f0d9b8d6fc9f966c90
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-25 14:38:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -186,6 +186,28 @@ app.get('/:iasName',function(req,res){
         }
     });
 });
+app.get('/:iasName',function(req,res){
+    pool.query("SELECT * FROM article WHERE id="+ req.params.iasName, function(err,result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            
+            if(result.rows.length===0)
+            {  
+                res.status(404).send('Article not found');
+            }
+            else
+            {
+                var articlesData=result.rows[0];
+                res.send(createTemplate(articlesData));
+            }
+            
+        }
+    });
+});
   
   
   
