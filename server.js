48f772a056b2fc7f7dcb71c3426e9ab8da6208b3
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-25 12:58:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -163,7 +163,28 @@ app.get('/test-db', function(req,res){
     
 });//get ends here
 
-app.get('/:articleName',)
+app.get('articles/:articleName',function(req,res){
+    pool.query("SELECT * FROM article WHERE tile="+req.params.articleName, function(err,result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            
+            if(result.rows.length===0)
+            {  
+                res.status(404).send('Article not found');
+            }
+            else
+            {
+                var articlesData=result.rows[0];
+                res.send(createTemplate(articlesData));
+            }
+            
+        }
+    });
+});
   
   
   
