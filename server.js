ea6506980902a5f221552c209d342f82dfd83f11
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-25 11:20:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,11 +144,10 @@ var config={
 var pool= new Pool(config);
 app.get('/:articleName', function(req,res){
 
-    pool.query("SELECT * FROM article WHERE title=$1", req.params.articleName), 
-    function(err,result)
-    {
+    pool.query("SELECT * FROM article WHERE title=$1", req.params.articleName,  function(err,result){
+    
         
-        if(err){
+       /* if(err){
                  res.status(500).send(err.toStringify());
                }
        if(result.rows.length===0)
@@ -156,13 +155,13 @@ app.get('/:articleName', function(req,res){
            res.status(404).send('Article not found');
        }
        
-       else{
+       else{*/
            var articleData= result.rows[0];
            res.send(createTemplate(articleData));
-           }
-    
-    };
-});   
+           //}
+    }
+
+);   });
   
   
   
