c349933bd76cbdaebed559a92b220f456fd45eef
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-02-25 10:47:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ var config={
     database:'internetadvancedsolutions',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:'process.env.DB_PASSWORD'
+    password:process.env.DB_PASSWORD
 }
 var pool= new Pool(config);
 app.get('/:articleName', function(req,res){
