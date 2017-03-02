dec6dd9555d69ee6ef65aba084e7ed0c6e027ff6
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-02 20:29:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ function createTemplate1(data){
         ${content1}
 		</td></tr>
 		<tr><td>Start Date :</td><td>
-		 ${content2.toString()}
+		 ${content2}
 		 </td></tr>
 	   <tr><td>Fees Rs :</td><td>
 		  ${price}
