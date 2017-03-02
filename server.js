2fba125169b531b9d5b0fb19d3d809a6c6919297
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-02 19:40:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.post('create-user',function(req,res){
 
 app.get('/test-db', function(req,res){
 
-    pool.query("SELECT * FROM test",  function(err,result){
+    pool.query("SELECT * FROM course",  function(err,result){
     
         
         if(err){
