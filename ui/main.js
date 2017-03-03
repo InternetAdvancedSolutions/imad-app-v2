d9b4fa6e1a129a24c05417c82ca167909b1a0b01
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-03 18:14:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@
         { 
              var counter =request.responseText;
              var span=document.getElementById("sp");
-             span.innerHTML=" visits "+counter;
+             span.innerHTML=counter;
             
         }
         
