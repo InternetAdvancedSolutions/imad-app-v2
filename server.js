c44dc3ebd314b6fca5f01650a87a02328232941c
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-03 18:26:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bodyparser=require('body-parser');
 
 var app = express();
 app.use(morgan('combined'));
+app.use('body-parser').JSON();
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
