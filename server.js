058bd691861023f27f1635cd5a1ab4b611a00553
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-03 09:19:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -227,9 +227,9 @@ app.get('/:articleName',function(req,res){
 
   
   
-app.get('/core', function(req,res){
+app.get('/PHP', function(req,res){
 
-    pool.query("SELECT * FROM course WHERE id= $1", [req.params.core],  function(err,result){
+    pool.query("SELECT * FROM course WHERE subject= $1", [req.params.PHP],  function(err,result){
     
         
         if(err){
