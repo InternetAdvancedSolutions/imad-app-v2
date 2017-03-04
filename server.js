a6da40ec98e21ae38e5b8ead34715244622effce
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-04 20:43:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ var pool= new Pool(config);
 
 //function to create a new user
 
-app.post('create-user',function(req,res){
+app.post('/create-user',function(req,res){
    var username=req.body.username;
    var password=req.body.password;
    var salt=crypto.RandomBytes(128).toString('hex');
