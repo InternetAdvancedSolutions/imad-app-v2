d499c91b0a4276db667cdaffe0c55f9b77858544
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-09 00:01:36 +0530

    Create register.js

---
+++ ui/register.js
@@ -0,0 +1,33 @@
+var reg_btn= document.getElementById('create');
+reg_btn.onclick=function(){
+
+// Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  document.getElementById('register').innerHTML='Registered';
+                 // register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                    document.getElementById('register').innerHTML='Register';
+                 // register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
