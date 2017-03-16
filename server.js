cf2c24a67179fc615f6b9a5ea612f73dc56a5132
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-17 00:42:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(session(
     {
         secret: 'someRandomSecretValue',
   
-        cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+        cookie: { maxAge: 1000 * 60 * 60 * 24 * 30},
          
     }  
 ));         
