e1c506f8cc4412e4a8f8224a2a3c095f13b7e7c0
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-17 01:12:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 
 app.use(session(
-    {
+    {   resave:'false',
         secret: 'someRandomSecretValue',
         cookie: { maxAge: 1000 * 60 * 60 * 24 * 30},
          
