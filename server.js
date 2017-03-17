184a13806fc553d83b555695b3b5d43eed83600e
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-17 11:33:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -294,7 +294,7 @@ app.get('/:articleName',function(req,res){
     if (req.session && req.session.auth && req.session.auth.userId ) {
        // Load the user object
       // pool.query('SELECT * FROM "article" WHERE id = $1', [req.session.auth//.userId], function (err, result) {
-      pool.query("SELECT * FROM 'article' WHERE hcode= $1", [req.params.articleName], function(err,result){
+      pool.query('SELECT * FROM "article" WHERE hcode= $1', [req.params.articleName], function(err,result){
            if (err) {
               res.status(500).send(err.toString());
            } else {
