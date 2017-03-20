72d2e3c30210d493f3f0769b83dfd4fc523d9ae3
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-03-20 22:29:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,10 +60,10 @@ function createArticle(data,idd){
         //var ids =idd;
    // var htmlarticle=`<a href="/aa/archive/${ids}" class="fontcolor1">${article1}</a>`;
    var htmlarticle=`
-   <a href="/aa/archive/7" class="fontcolor1">Coding Tips</a><br>
+   <a href="/aa/archive/8" class="fontcolor1">Coding Tips</a><br>
    <a href="/aa/archive/6" class="fontcolor1">cyber means?</a><br>
    <a href="/aa/archive/5" class="fontcolor1">what is 404?</a><br>
-   <a href="/aa/archive/8" class="fontcolor1">javaScript</a><br>
+   <a href="/aa/archive/7" class="fontcolor1">javaScript</a><br>
    <a href="/aa/archive/0" class="fontcolor1">what is html?</a><br>
    <a href="/aa/archive/1" class="fontcolor1">what is xml?</a><br>
    <a href="/aa/archive/2" class="fontcolor1">what is css?</a><br>
