9531a42140849621b572578a81743dfe8da0e173
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-04-12 01:11:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ function createTemplate(data1,data2){
               var topic1=document.getElementById("topic").value;
               var user_post1=document.getElementById("user_post").value; 
               var topic=topic1.trim();
-              var user_post.trim();
+              var user_post=user_post1.trim();
               var username_post= "${username}"; 
               console.log(topic);
               console.log(user_post);
