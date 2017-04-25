dfa3b2db5aec934d65155c51e92f45f7cecbb8b7
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-04-25 23:54:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,10 +55,13 @@ app.get('/counter',function(req,res){
 });
 
 app.post('/file_upload', function (req, res) {
+   //console.log(req.files.file.name);
+   //console.log(req.files.file.path);
+   //console.log(req.files.file.type);
+   var file = __dirname + "/" + req.files.file.name;
    console.log(req.files.file.name);
    console.log(req.files.file.path);
    console.log(req.files.file.type);
-   var file = __dirname + "/" + req.files.file.name;
    
    fs.readFile( req.files.file.path, function (err, data) {
       fs.writeFile(file, data, function (err) {
