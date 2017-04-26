8a7e4b811ee9c297d531829f7d3a08d533c3dca7
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-04-26 11:14:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.get('/ui/:fileName', function (req, res) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  console.log("Cookies: ", req.cookies);
 });
 
 
